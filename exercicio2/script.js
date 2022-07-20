b068cc691739d8c97cf29ce5553c54c476cb257e
Luara <luaraap7@gmail.com>
2022-07-20 02:56:08 -0300

    exercicio2/finalizado

---
+++ exercicio2/script.js
@@ -0,0 +1,24 @@
+// const cansada = prompt("Você está cansada?");
+// const pudim = prompt("Você gosta de pudim?");
+// const estudar = prompt("Você gosta de estudar?");
+// console.log(cansada,
+//     pudim,
+//     estudar)
+
+    //RESPOSTAS
+    let A = "Sim";
+    let B = "Não";
+    let C = "Sim";
+    console.log(A,
+        B,
+        C)
+       
+        const cansada = ("Você está cansada?");
+const pudim = ("Você gosta de pudim?");
+const estudar = ("Você gosta de estudar?");
+
+
+
+console.log(cansada, A)
+console.log(pudim, B)
+console.log(estudar, C)
