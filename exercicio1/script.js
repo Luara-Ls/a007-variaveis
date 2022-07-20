4f90ead2899ca40bf1b9ff86a6ec305505e97d43
Luara <luaraap7@gmail.com>
2022-07-20 02:01:56 -0300

    exercicio1finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,18 @@
+//  const nome = "Luara";
+// const idade = 31;
+
+// // console.log(typeof nome,
+// //     typeof idade)
+//     //Mensagem de erro no DevTools antes da atribuição dos valores, sem eles, o typeof não teria o que específicar. 
+
+//     const nome = prompt("Qual seu nome?");
+//     const idade = prompt("Qual sua idade?")
+//     console.log(nome)
+//     console.log(idade)
+// console.log(typeof nome,
+//     typeof idade)
+    //F: retornou como null
+    const nome = "Luara";
+    const idade = 31;
+
+    console.log("Olá", nome, "você tem", idade, "anos")
